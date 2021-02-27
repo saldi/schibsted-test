9addd47ab85d4de11be9458cccb467f2f8de4639
Tomasz Łabuz <javatrainings@gmail.com>
2021-02-27 21:35:51 +0100

    added dependency to Mockk library refactored versioning properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     kotlin("jvm") version "1.4.21"
 }
 
+var mockkVersion = "1.10.6"
+var junitVersion = "5.6.0"
+
 group = "edu.words"
 version = "1.0.0-SNAPSHOT"
 
@@ -12,7 +15,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
